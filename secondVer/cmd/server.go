19726c148ec1fb9c619a7c60f47b07acd80585d5
Reid <isreid.zhang@outlook.com>
2022-06-13 13:42:34 +0800

    fix banner typo

--- secondVer/cmd/server.go
+++ secondVer/cmd/server.go
@@ -33,7 +33,7 @@ const (
 
 func init() {
 	// print basic infomation
-	path, _ := filepath.Abs("resource/baner.txt")
+	path, _ := filepath.Abs("resource/banner.txt")
 	banner, _ := os.ReadFile(path)
 	fmt.Println(string(banner))
 }
